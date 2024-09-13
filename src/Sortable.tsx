1cddb62e7431d699a7dfe9a547f24e36e9ba2434
Mingyu Cui <mingyu.cui@robinhood.com>
2024-09-13 16:39:06 -0400

    menu ok

--- src/Sortable.tsx
+++ src/Sortable.tsx
@@ -35,6 +35,7 @@ import {
 
 import { createRange } from "./utilities";
 import { Item, List, Wrapper } from "./components";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 export interface Props {
   activationConstraint?: PointerActivationConstraint;
@@ -188,6 +189,66 @@ export function Sortable({
       isFirstAnnouncement.current = true;
     }
   }, [activeId]);
+
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button className="IconButton" aria-label="Customise options">
+          Open menu
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Tab <div className="RightSlot">⌘+T</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            New Window <div className="RightSlot">⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="DropdownMenuItem" disabled>
+            New Private Window <div className="RightSlot">⇧+⌘+N</div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Sub>
+            <DropdownMenu.SubTrigger className="DropdownMenuSubTrigger">
+              More Tools
+            </DropdownMenu.SubTrigger>
+            <DropdownMenu.Portal>
+              <DropdownMenu.SubContent
+                className="DropdownMenuSubContent"
+                sideOffset={2}
+                alignOffset={-5}
+              >
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Save Page As… <div className="RightSlot">⌘+S</div>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Create Shortcut…
+                </DropdownMenu.Item>
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Name Window…
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator className="DropdownMenu.Separator" />
+                <DropdownMenu.Item className="DropdownMenuItem">
+                  Developer Tools
+                </DropdownMenu.Item>
+              </DropdownMenu.SubContent>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Sub>
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.Separator className="DropdownMenuSeparator" />
+
+          <DropdownMenu.Label className="DropdownMenuLabel">
+            People
+          </DropdownMenu.Label>
+
+          <DropdownMenu.Arrow className="DropdownMenuArrow" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
 
   return (
     <DndContext
